da558d59ec4a2cff3ac0126ca66c3afa2de3b18b
bigdatalabsg <bigdatalabsg@gmail.com>
2023-10-15 21:05:12 +0800

    generateSchema.scala - Helper Class for Generting Schema

---
+++ src/main/scala/com/bigdatalabs/stable/utils/generateSchema.scala
@@ -0,0 +1,76 @@
+package com.bigdatalabs.stable.utils
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
+import java.io.{FileNotFoundException, IOException}
+
+class generateSchema {
+
+    def getStruct(args: String): StructType = {
+
+        var _schema: StructType = null
+
+        val spark = SparkSession.builder
+          .master("local[*]") //"spark://localhost:7077"
+          .appName("struct Generator")
+          .getOrCreate()
+
+        //Fetch Property File Path from Input Parameter
+        val _schemaFile = args
+
+        //Check for Propoerties File
+        try {
+            print("=======================================================================\n")
+            println("RESOURCE FILE:" + _schemaFile)
+            print("=======================================================================\n")
+        } catch {
+            case ex: FileNotFoundException => {
+                println(ex.printStackTrace())
+                System.exit(1)
+            }
+            case ex: IOException => {
+                println(ex.printStackTrace())
+                System.exit(2)
+            }
+        }
+
+        def _inferType(field: String) = field.split(":")(1) match {
+            case "byte" => ByteType
+            case "short" => ShortType
+            case "integer" => IntegerType
+            case "long" => LongType
+            case "float" => FloatType
+            case "double" => DoubleType
+            //case "decimal" => DecimalType
+            case "string" => StringType
+            case "binary" => BinaryType
+            case "boolean" => BooleanType
+            case "timestamp" => TimestampType
+            case "date" => DateType
+            case _ => StringType
+        }
+
+        try {
+            //Read Header from Schema File
+            val _header = spark.read
+              .format("csv")
+              .load(_schemaFile.toString)
+              .first()
+              .mkString(",")
+
+            //Build Schema
+            _schema = StructType(_header.split(",")
+              .map(_colName => StructField(_colName.substring(0, _colName.indexOf(":")), _inferType(_colName), nullable = true)))
+
+        } catch {
+            case ex: Exception => {
+                System.out.println(ex.printStackTrace())
+                System.exit(2)
+            }
+        }
+
+        return _schema
+
+    }
+}
