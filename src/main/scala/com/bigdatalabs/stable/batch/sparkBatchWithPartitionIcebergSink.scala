815103cbf860658e3c74157d6063174c40fae2b3
bigdatalabsg <bigdatalabsg@gmail.com>
2023-10-21 17:32:30 +0800

    sparkBatchWithPartitionIcebergSink.scala - Handler for Dynamic Partition Parameters (one ore more) sparkBatchWithPartitionIcebergSink.cfg - changed the Config Parametrs for Partion Columns

--- src/main/scala/com/bigdatalabs/stable/batch/sparkBatchWithPartitionIcebergSink.scala
+++ src/main/scala/com/bigdatalabs/stable/batch/sparkBatchWithPartitionIcebergSink.scala
@@ -3,6 +3,8 @@ package com.bigdatalabs.stable.batch
 import com.bigdatalabs.stable.utils.generateSchema
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
 
 import java.io.{FileNotFoundException, IOException}
 import scala.io.{BufferedSource, Source}
@@ -25,7 +27,7 @@ object sparkBatchWithPartitionIcebergSink {
 
         var _dbName: String = null
         var _tgtTblName: String = null
-        var _partitionCol: String = null
+        var _partitionColumnSeq: Seq [String]   = null //Sequence of partition Columns in Correct Order
 
         var _SQL: String = null
 
@@ -88,10 +90,16 @@ object sparkBatchWithPartitionIcebergSink {
         _quoteChar = _configMap("quoteChar")
 
         _tgtSchemaFile = _configMap("tgtSchemaFile")
-
         _dbName = _configMap("dbName")
         _tgtTblName = _configMap("tgtTblName")
-        _partitionCol = _configMap("partitionCol")
+
+        //Partition Column Set
+        _partitionColumnSeq = _configMap("partitionColumnSeq").split(",").toSeq
+
+        if(_partitionColumnSeq==null) {
+            println("Partition Columns Not Defined - Exiting")
+            System.exit(3)
+        }
 
         _SQL = _configMap("SQL")
 
@@ -121,29 +129,33 @@ object sparkBatchWithPartitionIcebergSink {
 
         //Create Temp table
         df_src.cache()
-        df_src.createOrReplaceTempView("genericTempTable")
+        df_src.createOrReplaceTempView(viewName="genericTempTable")
 
         val df_tgt = spark.sql(_SQL.stripMargin)
 
+        //Determine Partition Columns
+        val _partColumns = _partitionColumnSeq
+        val _partColumnNames = _partColumns.map(_colName=>col(_colName))
+
         try {
-            println(s"Start Loading Iceberg")
+            println(s"""Start Loading Iceberg""")
             //Append new Data
             df_tgt
-              .sortWithinPartitions(_partitionCol) //("station")
+              .sortWithinPartitions(_partColumnNames:_*)
               .writeTo(_dbName + "." + _tgtTblName)
               .append()
 
             println("SRC COUNT :" + df_tgt.count().toString)
-            println(s"Loading Completed")
+            println(s"""Loading Completed""")
         }
         catch {
             case ex: Exception =>
-                System.out.println("Loading Failed")
+                System.out.println(s"""Loading Failed""")
                 System.out.println(ex.printStackTrace())
 
         } finally {
             println("SRC COUNT :" + df_tgt.count().toString)
-            println(s"Loading Completed")
+            println(s"""Loading Completed""")
             spark.stop()
         }
     }
