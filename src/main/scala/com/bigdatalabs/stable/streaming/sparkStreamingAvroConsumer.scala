b204c7e2e2cb4f31bde0c19bb54695fc41db707a
bdluser <bigdatalabsg@gmail.com>
2024-08-25 23:41:10 +0800

    Refactored Code, Removed Unsed declarations

--- src/main/scala/com/bigdatalabs/stable/streaming/sparkStreamingAvroConsumer.scala
+++ src/main/scala/com/bigdatalabs/stable/streaming/sparkStreamingAvroConsumer.scala
@@ -6,131 +6,106 @@
 
 package com.bigdatalabs.stable.streaming
 
+import com.bigdatalabs.stable.utils.configGenerator
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.avro.functions.from_avro
 import org.apache.spark.sql.functions.col
 
-import java.io.{FileNotFoundException, IOException}
 import java.nio.file.{Files, Paths}
-import scala.io.{BufferedSource, Source}
 
 object sparkStreamingAvroConsumer {
 
-    def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-        var _configFile: BufferedSource = null
+    var _configParams: Map[String, String] = null
 
-        var _brokers: String = null
-        var _subsTopic: String = null
-        var _offSet: String =null
-        var _srcSchemaFile: String = null
-        var _avroSchemaFile: String = null
-        var _tgtSchemaFile: String = null
-        var _pubsTopic: String = null
-        var _groupId: String = null
-        var _microbatchSecs: Int = 0
-        var _streamFormat: String = null
-        var _lineSplitterChar: String = null
-        var _quoteChar: String = null
-        var _delimiterChar: String = null
+    var _brokers: String = null
+    var _subsTopic: String = null
+    var _offSet: String = null
+    var _srcSchemaFile: String = null
+    var _avroSchemaFile: String = null
+    var _tgtSchemaFile: String = null
+    var _pubsTopic: String = null
+    var _groupId: String = null
 
-        //Spark Context
-        val spark = SparkSession.builder()
-          .master("local[*]")
-          .appName("spark structured streaming")
-          .getOrCreate()
+    //Spark Context
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("spark structured streaming")
+      .getOrCreate()
 
-        //Set Logging Level
-        spark.sparkContext.setLogLevel("WARN")
+    //Set Logging Level
+    spark.sparkContext.setLogLevel("WARN")
 
-        //Fetch Property File Path from Input Parameter
-        val _prop_file_path = args(0)
+    //Fetch Property File Path from Input Parameter
+    val _prop_file_path: String = args(0)
 
-        //Check for Properties File
-        try {
-            println("=======================================================================\n")
-            println("SPARK SERVICE NAME:" + this.getClass.getName.toUpperCase())
-            print("=======================================================================\n")
-            println("RESOURCE FILE:" + _prop_file_path)
-            print("=======================================================================\n")
-
-            _configFile = Source.fromFile(_prop_file_path)
-
-        } catch {
-            case ex: FileNotFoundException =>
-                println(ex.printStackTrace())
-                System.exit(1)
-
-            case ex: IOException =>
-                println(ex.printStackTrace())
-                System.exit(2)
-
-        }
-
-        //Read Application Config
-        val _configMap = _configFile.getLines().filter(line => line.contains("::")).map { line =>
-            val _configTokens = line.split("::")
-            if (_configTokens.size == 1) {
-                _configTokens(0) -> ""
-            } else {
-                _configTokens(0) -> _configTokens(1)
-            }
-        }.toMap
-
-        //Initialize Variables
-        _brokers = _configMap("brokers")
-        _subsTopic = _configMap("subsTopic")
-        _offSet = _configMap("offSet")
-        _srcSchemaFile = _configMap("srcSchemaFile")
-        _avroSchemaFile = _configMap("avroSchemaFile")
-        _tgtSchemaFile = _configMap("tgtSchemaFile")
-        _pubsTopic = _configMap("pubsTopic")
-        _streamFormat = _configMap("fileFormat")
-        _groupId = _configMap("groupId")
-        _microbatchSecs = _configMap("microbatchSecs").toInt
-        _lineSplitterChar = _configMap("lineSplitterChar")
-        _delimiterChar = _configMap("delimiterChar")
-        _quoteChar = _configMap("quoteChar")
-
-        println("brokers :" + _brokers)
-        println("subsTopic :" + _subsTopic)
-        println("srcSchemaFile :" + _srcSchemaFile)
-        println("avroSchemaFile :" + _avroSchemaFile)
-        println("tgtSchemaFile :" + _tgtSchemaFile)
-        println("pubsTopic :" + _pubsTopic)
-        println("groupId :" + _groupId)
-        println("microbatchSecs :" + _microbatchSecs)
-        println("lineSplitterChar :" + _lineSplitterChar)
-        println("delimiterChar :" + _delimiterChar)
-        println("quoteChar :" + _quoteChar)
-
-        //Read from JSON Message Stream
-        val df_stream = spark
-          .readStream
-          .format("kafka")
-          .option("kafka.bootstrap.servers", _brokers)
-          .option("subscribe", _subsTopic)
-          .option("startingOffsets", _offSet) // From starting
-          .load()
-
-        //Inspect Schema
-        //df_stream.printSchema()
-
-        //Pull Schema
-        val _avroSchema = new String(
-            Files.readAllBytes(Paths.get(_avroSchemaFile)))
-
-        //
-        val df_from_avro = df_stream.select(
-            from_avro(col("value"),_avroSchema).alias("data")
-        ).select("data.*")
-
-        df_from_avro.writeStream
-          .format("console")
-          .outputMode("append")
-          .option("mode", value="PERMISSIVE")
-          .option("truncate", value = false)
-          .start()
-          .awaitTermination()
+    if (_prop_file_path == null) {
+      println("Property File Not Found - Exiting")
+      System.exit(4)
     }
+
+    //Check for Properties File
+    _configParams = new configGenerator().getParams(_prop_file_path)
+
+    if (_configParams == null) {
+      println("Check Configuration File - Exiting")
+      System.exit(1)
+    }
+
+    //Initialize Variables
+    _brokers = _configParams("brokers")
+    _subsTopic = _configParams("subsTopic")
+    _offSet = _configParams("offSet")
+    _groupId = _configParams("groupId")
+
+    _pubsTopic = _configParams("pubsTopic")
+
+    _srcSchemaFile = _configParams("srcSchemaFile")
+    _avroSchemaFile = _configParams("avroSchemaFile")
+    _tgtSchemaFile = _configParams("tgtSchemaFile")
+
+    //Fetch AVRO schema
+    val _avroSchema = new String(
+      Files.readAllBytes(Paths.get(_avroSchemaFile)))
+
+
+    print("=============================================================================================================\n")
+    println("SPARK SERVICE NAME:" + this.getClass.getName.toUpperCase().dropRight(1))
+    print("=============================================================================================================\n")
+    println("RESOURCE FILE:" + _prop_file_path)
+    print("=============================================================================================================\n")
+    println("SCHEMA FILE :" + _srcSchemaFile)
+    print("============================================= SERVICE PARAMETERS ============================================\n")
+    println("brokers :" + _brokers)
+    println("subsTopic :" + _subsTopic)
+    println("groupId :" + _groupId)
+    println("offSet :" + _offSet)
+    print("=============================================================================================================\n")
+
+    //Read from JSON Message Stream
+    val df_stream = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", _brokers)
+      .option("subscribe", _subsTopic)
+      .option("startingOffsets", _offSet) // From starting
+      .option("failOnDataLoss", value = false)
+      .load()
+
+    //Inspect Schema
+    //df_stream.printSchema()
+
+    val df_from_avro = df_stream.select(
+      from_avro(col("value"), _avroSchema).alias("data")
+    ).select("data.*")
+
+    df_from_avro.writeStream
+      .format("console")
+      .outputMode("append")
+      .option("mode", value = "PERMISSIVE")
+      .option("truncate", value = false)
+      .start()
+      .awaitTermination()
+  }
 }
