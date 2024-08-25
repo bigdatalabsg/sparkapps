fbf67bc9a5eaf5f0ace14b4c4cb5a10c60bb53f2
bdluser <bigdatalabsg@gmail.com>
2024-08-25 23:46:04 +0800

    Refactored Code, Removed Unsed declarations

--- src/main/scala/com/bigdatalabs/stable/batch/sparkBatchIcebergSink.scala
+++ src/main/scala/com/bigdatalabs/stable/batch/sparkBatchIcebergSink.scala
@@ -6,156 +6,147 @@
 
 package com.bigdatalabs.stable.batch
 
-import com.bigdatalabs.stable.utils.generateSchema
+import com.bigdatalabs.stable.utils.{configGenerator, preparedStatementGenerator, schemaGenerator}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-
-import java.io.{FileNotFoundException, IOException}
-import scala.io.{BufferedSource, Source}
 
 object sparkBatchIcebergSink {
 
-    def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-        //Variables
-        var _configFile: BufferedSource = null
+    //Variables
+    var _configParams: Map[String, String] = null
 
-        var _srcFileName: String = null
-        var _srcSchemaFile: String = null
-        var _fileFormat: String = null
-        var _inferSchemaFlag: String = null
-        var _headerFlag: String = null
-        var _delimiter: String = null
-        var _quoteChar: String = null
+    var _srcFileName: String = null
+    var _srcSchemaFile: String = null
+    var _fileFormat: String = null
+    var _inferSchemaFlag: String = null
+    var _headerFlag: String = null
+    var _delimiter: String = null
 
-        var _dbName: String = null
-        var _tgtTblName: String = null
-        var _partitionCol: String = null
+    //Database Block
+    var _dbName: String = null
+    var _tgtTblName: String = null
 
-        var _SQL: String = null
+    //SQL Block
+    var _SQLFilePath: String = null
+    var _SQLStmt: String = null
 
-        //Session
-        val spark = SparkSession.builder
-          .appName("spark-batch-iceberg-sink")
-          .master("local[*]")
-          .getOrCreate()
 
-        //Set Logging Level
-        spark.sparkContext.setLogLevel("WARN")
+    //Session
+    val spark = SparkSession.builder
+      .appName("spark-batch-iceberg-sink")
+      .master("local[*]")
+      .getOrCreate()
 
-        if (args.length < 1) {
-            System.err.println(
-                s"""
-                   |Usage: --resourcefile <file path>
+    //Set Logging Level
+    spark.sparkContext.setLogLevel("WARN")
+
+    if (args.length < 1) {
+      System.err.println(
+        s"""
+           |Usage: --resourcefile <file path>
                 """.stripMargin)
-            System.exit(1)
-        }
-
-        //Fetch Property File Path from Input Parameter
-        val _prop_file_path = args(0)
-
-        //Check for Properties File
-        try {
-            println("=======================================================================\n")
-            println("SPARK SERVICE NAME:" + this.getClass.getName.toUpperCase())
-            print("=======================================================================\n")
-            println("RESOURCE FILE:" + _prop_file_path)
-            print("=======================================================================\n")
-
-            _configFile = Source.fromFile(_prop_file_path)
-
-        } catch {
-            case ex: FileNotFoundException =>
-                println(ex.printStackTrace())
-                System.exit(1)
-
-            case ex: IOException =>
-                println(ex.printStackTrace())
-                System.exit(2)
-
-        }
-
-        //Read Application Config
-        val _configMap = _configFile.getLines().filter(line => line.contains("::")).map { line =>
-            val _configTokens = line.split("::")
-            if (_configTokens.size == 1) {
-                _configTokens(0) -> ""
-            } else {
-                _configTokens(0) -> _configTokens(1)
-            }
-        }.toMap
-
-        //Parameters
-        _srcFileName = _configMap("srcFileName")
-        _fileFormat = _configMap("fileFormat")
-        _srcSchemaFile = _configMap("srcSchemaFile")
-
-        _inferSchemaFlag = _configMap("inferSchemaFlag")
-        _headerFlag = _configMap("headerFlag")
-        _delimiter = _configMap("delimiter")
-        _quoteChar = _configMap("quoteChar")
-
-        _dbName = _configMap("dbName")
-        _tgtTblName = _configMap("tgtTblName")
-        _partitionCol = _configMap("partitionCol")
-
-        _SQL = _configMap("SQL")
-
-        //Generate Schema
-        val _srcSchema: StructType = new generateSchema().getStruct(_srcSchemaFile)
-
-        //Check Schema
-        if (_srcSchema == null) {
-            System.out.println("Schema Undefined - Exiting")
-            System.exit(3)
-        }
-
-        //_srcSchema.printTreeString()
-
-        //Read from Files
-        val df_raw = spark.read.format(_fileFormat)
-          .schema(_srcSchema)
-          .option("inferSchema", _inferSchemaFlag)
-          .option("header", _headerFlag)
-          .option("sep", _delimiter)
-          .option("quote", _quoteChar)
-          .load(_srcFileName)
-          .toDF()
-
-        //df_raw.printSchema()
-        System.out.println("Loading Data to Target")
-
-        df_raw.createOrReplaceTempView("genericTempTable")
-
-        val _DML =
-            s"""
-               |SELECT station,CAST(rep_date as date) ,CAST(latitude as double),CAST(longitude as double),name,CAST(temp as double)
-               |FROM stg_station
-               |""".stripMargin
-
-        val df_station = spark.sql(_SQL.stripMargin)
-
-        try {
-            println(s"Start Loading Iceberg")
-            //Append new Data
-            df_station
-              //              .sortWithinPartitions("station")
-              .writeTo(_dbName + "." + _tgtTblName)
-              .append()
-
-            println("SRC COUNT :" + df_station.count().toString)
-            println(s"Loading Completed")
-        }
-        catch {
-            case ex: Exception =>
-                System.out.println("Loading Failed")
-                System.out.println(ex.printStackTrace())
-
-        } finally {
-            println("SRC COUNT :" + df_station.count().toString)
-            println(s"Loading Completed")
-            spark.stop()
-        }
+      System.exit(1)
     }
+
+    //Fetch Property File Path from Input Parameter
+    val _prop_file_path: String = args(0)
+
+    if (_prop_file_path == null) {
+      println("Property File Not Found - Exiting")
+      System.exit(4)
+    }
+
+    //Check for Properties File
+    _configParams = new configGenerator().getParams(_prop_file_path)
+
+    if (_configParams == null) {
+      println("Check Configuration File - Exiting")
+      System.exit(1)
+    }
+
+    //Parameters
+    _srcFileName = _configParams("srcFileName")
+    _fileFormat = _configParams("fileFormat")
+    _srcSchemaFile = _configParams("srcSchemaFile")
+
+    _inferSchemaFlag = _configParams("inferSchemaFlag")
+    _headerFlag = _configParams("headerFlag")
+    _delimiter = _configParams("delimiter")
+
+    _dbName = _configParams("dbName")
+    _tgtTblName = _configParams("tgtTblName")
+
+    _SQLFilePath = _configParams("SQLFilePath")
+
+    //Check for Properties File
+    print("=============================================================================================================\n")
+    println("SPARK SERVICE NAME:" + this.getClass.getName.toUpperCase().dropRight(1))
+    print("=============================================================================================================\n")
+    println("RESOURCE FILE:" + _prop_file_path)
+    print("=============================================================================================================\n")
+    println("SQL FILE:" + _SQLFilePath)
+    print("=============================================================================================================\n")
+    println("SCHEMA FILE :" + _srcSchemaFile)
+    print("============================================= SERVICE PARAMETERS ============================================\n")
+    println("dbName :" + _dbName)
+    println("tgtTblName :" + _tgtTblName)
+    print("=============================================================================================================\n")
+
+    //Generate Schema
+    val _srcSchema: StructType = new schemaGenerator().getSchema(_srcSchemaFile)
+
+    //Check Schema
+    if (_srcSchema == null) {
+      System.out.println("Schema Undefined - Exiting")
+      System.exit(4)
+    }
+
+    //Fetch SQL
+    _SQLStmt = new preparedStatementGenerator().getStatement(_SQLFilePath)
+
+    if (_SQLStmt == null) {
+      println("Undefined SQL - Exiting")
+      System.exit(4)
+    }
+
+    //_srcSchema.printTreeString()
+
+    //Read from Source
+    spark.read.format(_fileFormat)
+      .schema(_srcSchema)
+      .option("inferSchema", _inferSchemaFlag)
+      .option("header", _headerFlag)
+      .option("sep", _delimiter)
+      .load(_srcFileName)
+      .toDF().createOrReplaceTempView("genericTempView")
+
+    //Work Table
+    val df_work = spark.sql(_SQLStmt.stripMargin)
+
+    try {
+      println(s"Start Loading Iceberg")
+      //Append new Data
+      df_work
+        .writeTo(_dbName + "." + _tgtTblName)
+        .append()
+
+      //Validate Load
+      val df_tgt = spark.table(_dbName + "." + _tgtTblName)
+
+      println("SRC COUNT :" + df_work.count().toString)
+
+      println("TGT COUNT :" + df_tgt.count().toString)
+
+      println(s"Loading Completed")
+    }
+    catch {
+      case ex: Exception =>
+        System.out.println("Loading Failed")
+        System.out.println(ex.printStackTrace())
+    } finally {
+      spark.stop()
+    }
+  }
 }
