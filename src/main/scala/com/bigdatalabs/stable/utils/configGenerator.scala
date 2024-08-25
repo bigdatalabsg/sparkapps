af2e64cac85c2e3ccc86bc3e0613769acd0d3f99
bdluser <bigdatalabsg@gmail.com>
2024-08-26 01:00:32 +0800

    new class for returning avro schema

---
+++ src/main/scala/com/bigdatalabs/stable/utils/configGenerator.scala
@@ -0,0 +1,39 @@
+package com.bigdatalabs.stable.utils
+
+/*
+* Author : Anand
+* Date : 09-Aug-2024
+* Description: Struct Generator
+*/
+
+import scala.io._
+
+class configGenerator {
+
+  def getParams(args: String): Map[String, String] = {
+
+    val _path: String = args
+
+    var _configFile: BufferedSource = null
+    var _configMap: Map[String, String] = null
+    var _configTokens: Array[String] = null
+
+    _configFile = Source.fromFile(_path)
+
+    //Read Application Config
+    _configMap = _configFile.getLines().filter(line => line.contains("::")).map { line =>
+      _configTokens = line.split("::")
+      if (_configTokens.length == 1) {
+        _configTokens(0) -> ""
+      } else {
+        _configTokens(0) -> _configTokens(1)
+      }
+    }.toMap
+
+    _configFile.close()
+
+    //return
+    _configMap
+
+  }
+}
