39c622605ff8f9185b68a587b4dca04e87a8e2b8
bdluser <bigdatalabsg@gmail.com>
2024-08-26 07:31:05 +0800

    moved to SQL Folder

--- resources/sql/flt_del_batch_load.sql
+++ resources/sql/flt_del_batch_load.sql
@@ -1,4 +1,4 @@
-SELECT `year`,`month`,dayofmonth,CAST (CAST(CONCAT(`year`,'-',`month`,'-',dayofmonth) AS DATE) AS string) AS fltdt,dayofweek,deptime,
-CAST(CONCAT(substring(deptime,1,2),"00") AS string) AS dephour, crsdeptime,arrtime,crsarrtime,uniquecarrier,flightnum,tailnum,
-actualelapsedtime,crselapsedtime,airtime,arrdelay,depdelay,origin,dest,distance,taxiin,taxiout,cancelled,cancellationcode,diverted,
-carrierdelay,weatherdelay,nasdelay,securitydelay,lateaircraftdelay,isarrdelayed,isdepdelayed FROM genericTempView
+SELECT `year`,`month`,dayofmonth,fltdt,dayofweek,deptime,dephour,depmin,crsdeptime,arrtime,crsarrtime,uniquecarrier,
+flightnum,tailnum,actualelapsedtime,crselapsedtime,airtime,arrdelay,depdelay,origin,dest,distance,taxiin,taxiout,
+cancelled,cancellationcode,diverted,carrierdelay,weatherdelay,nasdelay,securitydelay,lateaircraftdelay,
+isarrdelayed,isdepdelayed FROM genericTempView
