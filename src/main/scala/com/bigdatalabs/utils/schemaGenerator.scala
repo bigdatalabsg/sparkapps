456048fa8edf6e614d65b594e8d1de43d944b161
bdluser <bigdatalabsg@gmail.com>
2024-08-31 14:35:07 +0800

    removed spark dataframe to read schema file, replaceed with source

--- src/main/scala/com/bigdatalabs/utils/schemaGenerator.scala
+++ src/main/scala/com/bigdatalabs/utils/schemaGenerator.scala
@@ -7,19 +7,23 @@ package com.bigdatalabs.utils
 */
 
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.SparkSession
+
+import scala.io.Source
 
 class schemaGenerator {
 
     def getSchema(args: String): StructType = {
 
-        var _schema: StructType = null
-        val _schemaFile = args //Fetch Property File Path from Input Parameter
+        val _schemaFilePath: String = args
+        var _schemaFile: Source = null
 
-        val spark = SparkSession.builder
-          .master("local[*]") //"spark://localhost:7077"
-          .appName("data frame struct generator utility")
-          .getOrCreate()
+        var _schemaHeader: String = null
+
+        var _schema: StructType = null
+
+        _schemaFile = Source.fromFile(_schemaFilePath.trim)
+
+        _schemaHeader = _schemaFile.getLines().mkString.trim
 
         //Infer Schema, Split the String [<Column Name> : <Data Type> ] at ":"
         def _inferType(field: String) = field.split(":")(1) match {
@@ -39,25 +43,16 @@ class schemaGenerator {
         }
 
         try {
-            //Read Header from Schema File
-            val _header = spark.read
-              .format("csv")
-              .load(_schemaFile)
-              .first()
-              .mkString(",")
-
             //Build Schema
-            _schema = StructType(_header.split(",")
+            _schema = StructType(_schemaHeader.split(",")
               .map(_colName => StructField(_colName.substring(0, _colName.indexOf(":")), _inferType(_colName), nullable = true)))
-
-            //Schema Names
-//            val _colNames = _header.split(",")
-//              .map(colName => colName.substring(0, colName.indexOf(":")).toUpperCase()).toSeq
 
         } catch {
             case ex: Exception =>
                 System.out.println(ex.printStackTrace())
                 System.exit(2)
+        } finally {
+            _schemaFile.close()
         }
 
         //Return Struct Type Object
